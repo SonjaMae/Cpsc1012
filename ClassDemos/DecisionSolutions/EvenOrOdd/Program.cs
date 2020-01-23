2567cd455ad77f84c43cf34731470916c0bb3797
SonjaMae <59752342+SonjaMae@users.noreply.github.com>
2020-01-22 18:45:57 -0700

    m

---
+++ ClassDemos/DecisionSolutions/EvenOrOdd/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int number;                     
+            const int Two = 2;     //this is a non-changeable variable                
+            string inputValue;
+
+            Console.Write("Enter first number:\t");
+            inputValue = Console.ReadLine();
+            number = int.Parse(inputValue);
+
+            if ((number % Two) == 0)
+            {
+                //true path coding block
+                //true means the condition on the if statement is true
+                Console.WriteLine($"Your number {number} is even.");
+            }
+            else
+            {
+                //false path coding block
+                //false means the condition on the if statement is false
+                Console.WriteLine($"Your number {number} is odd.");
+            }//eof
+        }//eom
+    }//eoc
+}//eon
