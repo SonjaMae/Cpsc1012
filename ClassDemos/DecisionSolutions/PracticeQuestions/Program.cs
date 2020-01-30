b1c7a3ad163b7b5c6463255b5d995635eb98565a
SonjaMae <59752342+SonjaMae@users.noreply.github.com>
2020-01-30 13:42:50 -0700

    practiceQuestions

--- ClassDemos/DecisionSolutions/PracticeQuestions/Program.cs
+++ ClassDemos/DecisionSolutions/PracticeQuestions/Program.cs
@@ -15,9 +15,11 @@ namespace PracticeQuestions
              * display positive if > 0
              * display negative if < 0
              * display zero if = 0*/
-            Console.WriteLine("Please enter a number: \t");
-            Console.ReadLine();
-            double theNumber = theNumber.Parse(string);
+            int theNumber;
+            string inputValue;
+            Console.WriteLine("Please enter a number:\t");
+            inputValue = Console.ReadLine();
+            theNumber = int.Parse(inputValue);
 
             if (theNumber > 0)
             {
@@ -31,12 +33,112 @@ namespace PracticeQuestions
             {
                 Console.WriteLine("Zero");
             }
+            Console.ReadKey();
+
 
             //Question 2
+            /*prompt for the user's age
+             *display admission amount for their age group*/
+            int userAge;
+            Console.WriteLine("Please enter your age:\t");
+            inputValue = Console.ReadLine();
+            userAge = int.Parse(inputValue);
+
+            if (userAge < 6)
+            {
+                Console.WriteLine("Your admission is free!");
+            }
+            else if (userAge < 17)
+            {
+                Console.WriteLine("Your admission is $9.80.");
+            }
+            else if (userAge < 54)
+            {
+                Console.WriteLine("Your admission is $11.35.");
+            }
+            else if (userAge > 55)
+            {
+                Console.WriteLine("Your admission is $10.00.");
+            }
+            Console.ReadKey();
+
 
             //Question 3
+            /*prompt for a student's name and their mark
+             * display the student's name and their letter grade based on their mark
+             * a = 100-90
+             * b = 89-80
+             * c = 79-70
+             * d = 69-50
+             * f = 49-0*/
+            string numberGrade;
+            int percentageRange;
+            string studentName;
+
+            Console.WriteLine("Please enter the student's name:\t");
+            studentName = Console.ReadLine();
+
+            Console.WriteLine("Now please enter their grade as a number:\t");
+            numberGrade = Console.ReadLine();
+            percentageRange = int.Parse(numberGrade);
+
+            if (percentageRange >= 90 && percentageRange <= 100)
+            {
+                Console.WriteLine($"The student {studentName}'s numerical grade of {numberGrade}% would be an A letter grade.");
+            }
+            else if (percentageRange >= 80 && percentageRange <= 89)
+            {
+                Console.WriteLine($"The student {studentName}'s numerical grade of {numberGrade}% would be a B letter grade.");
+            }
+            else if (percentageRange >= 70 && percentageRange <= 79)
+            {
+                Console.WriteLine($"The student {studentName}'s numerical grade of {numberGrade}% would be a C letter grade.");
+            }
+            else if (percentageRange >= 50 && percentageRange <= 69)
+            {
+                Console.WriteLine($"The student {studentName}'s numerical grade of {numberGrade}% would be a D letter grade.");
+            }
+            else if (percentageRange >= 0 && percentageRange <= 49)
+            {
+                Console.WriteLine($"The student {studentName}'s numerical grade of {numberGrade}% would be an F letter grade.");
+            }
+            else
+            {
+                Console.WriteLine($"The entered numerical grade {numberGrade} is invalid.");
+            }
+            Console.ReadKey();
+
 
             //Question 4
+            /*compute the income tax due on a taxable income entered by the user
+             * prompt for the user's taxable income
+             * display the income tax due on it*/
+            string taxableIncome;
+            double incomeTax;
+            double taxableIncomeInt;
+            string msg = "";
+
+            Console.WriteLine("Please enter your taxable income:\t");
+            taxableIncome = Console.ReadLine();
+            taxableIncomeInt = int.Parse(taxableIncome);
+
+            if (taxableIncomeInt < 50000)
+            {
+                incomeTax = taxableIncomeInt * 0.05;
+                msg = $"The income tax owed is {incomeTax}.";
+            }
+            else if (taxableIncomeInt >= 50000 && taxableIncomeInt < 100000)
+            {
+                incomeTax = 2500 + (taxableIncomeInt * 0.07);
+                msg = $"The income tax owed is {incomeTax}.";
+            }
+            else if (taxableIncomeInt >= 100000)
+            {
+                incomeTax = 6000 + (taxableIncomeInt * 0.09);
+                msg = $"The income tax owed is {incomeTax}.";
+            }
+            Console.WriteLine(msg);
+            Console.ReadKey();
         }
     }
 }
