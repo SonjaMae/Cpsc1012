6b362a4623601188c3c7577b65b975040a0ad319
SonjaMae <59752342+SonjaMae@users.noreply.github.com>
2020-02-26 18:40:11 -0700

    b

--- ClassDemos/IterationSolution/MenuLoop/Program.cs
+++ ClassDemos/IterationSolution/MenuLoop/Program.cs
@@ -55,44 +55,47 @@ namespace MenuLoop
                         }
                     case "C":
                         {
-                            //the sum of squares
-                            //enter positive integer number (validate)
-                            //loop n times where n is the integer number (1 to n)
-                            //within the loop your calculation is total = loopnumber * loopnumber
-                            //after the loop, display your integer number and its sum of squares
-                            //example: 
-                                    //entered 4
-                                    //do the loop 4 times
-                                    //1st time is 1 * 1
-                                    //2nd time is 2 * 2
-                                    //3rd time is 3 * 3
-                                    //4th time is 4 * 4
-                                    //final total = 30 (16+9+4+1=30)
-                            Console.WriteLine("You chose to play Sum of Squares.\n");
 
-                            string inputInteger;
-                            int loopNumber = 1;
-                            int totalValue = 0;
-                            //int finalTotal = 0;
-
-                            Console.Write("Enter a positive integer value:\t");
-                            inputInteger = Console.ReadLine();
-                            int inputIntegerInt = int.Parse(inputInteger);
-
-                            do
+                            //create a shared input method to obtain an integer value
+                            int number = GetIntegerInput("Enter a number greater than 0");
+                            if (number < 1)
                             {
-                                //totalValue = totalValue + loopNumber * loopNumber;
-                                //the line below does the same thing except in a shorter fashion
-                                //the += says "add the right side to the left side"
-                                totalValue += loopNumber * loopNumber;
+                                Console.WriteLine($"{number} is not greater than 0. Unable to do Sum of Squares.");
+                            }
+                            else
+                            {
+                                int sumofsquares = 0;
+                                sumofsquares = SumofSquaresMethod(number);
+                                Console.WriteLine($"{sumofsquares} is the sum of squares for {number}");
+
+                            }
+
+
+                           // Console.WriteLine("You chose to play Sum of Squares.\n");
+
+                           // string inputInteger;
+                           // int loopNumber = 1;
+                           // int totalValue = 0;
+                           // //int finalTotal = 0;
+
+                           // Console.Write("Enter a positive integer value:\t");
+                           // inputInteger = Console.ReadLine();
+                           // int inputIntegerInt = int.Parse(inputInteger);
+
+                           // do
+                           // {
+                           //     //totalValue = totalValue + loopNumber * loopNumber;
+                           //     //the line below does the same thing except in a shorter fashion
+                           //     //the += says "add the right side to the left side"
+                           //     totalValue += loopNumber * loopNumber;
                                 
-                                loopNumber++;
+                           //     loopNumber++;
 
-                            } while (loopNumber <= inputIntegerInt);
+                           // } while (loopNumber <= inputIntegerInt);
 
-                           // finalTotal = totalValue + totalValue + totalValue + totalValue;
+                           //// finalTotal = totalValue + totalValue + totalValue + totalValue;
 
-                            Console.WriteLine($"\nThe total square sum of your input value, {inputInteger}, is {totalValue}.\n\n");
+                           // Console.WriteLine($"\nThe total square sum of your input value, {inputInteger}, is {totalValue}.\n\n");
 
                             break;
                         }
@@ -115,28 +118,61 @@ namespace MenuLoop
         }//eom Main
 
         //the syntax for a method requires:
-            //accesstype returndatatype MethodName([list of parameters])
-            //^ that first line is called a method header
-            //{
-            //  your method code
-            //} //eom
+        //accesstype returndatatype MethodName([list of parameters])
+        //^ that first line is called a method header
+        //{
+        //  your method code
+        //} //eom
 
         //accesstype:
-            //public or private
-            //protected, internal, have static reference
+        //public or private
+        //protected, internal, have static reference
 
         //returndatatype:
-            //if you're returning nothing, use the keyword void
-            //otherwise, return a single value (such as int, double, decimal, DateTime, etc.)
+        //if you're returning nothing, use the keyword void
+        //otherwise, return a single value (such as int, double, decimal, DateTime, etc.)
 
         //MethodName:
-            //continuous string of characters which will be used within your program code to reference this coding block
-            //it's best to use a meaningful name
+        //continuous string of characters which will be used within your program code to reference this coding block
+        //it's best to use a meaningful name
 
         //[list of parameters]
-            //this is a set of local variables that will be used to receive values from the calling statement
-            //a parameter is a set of datatype and variablename
-            //parameters are separated by using ,
+        //this is a set of local variables that will be used to receive values from the calling statement
+        //a parameter is a set of datatype and variablename
+        //parameters are separated by using ,
+
+
+
+        //local variables are NOT automatically passed to any called method
+
+        //method dictates to the calling statement, not the other way around
+
+        //avoid having method name being the same as a variable name
+
+        //methods can be divided into functions or subroutines:
+            //functions return a value
+            //subroutines don't return anything
+
+        //method stub = a method with only the outline and no content
+
+
+
+        //how to pass data to / return data from a method:
+        
+        //pass by value = when changes made to an original document are unavailable
+        
+            //to pass data to a method:
+        //MethodName (drivervariable, optionalothervariable, etc)
+                    //^these variables on the calling statement are called arguments
+        //MethodName (int localcopyofdrivervariable, string localcopyofdrivervariable)
+                    //^these variables on the method are called parameters
+                    //parameters are locally declared, meaning they die at the end of the method
+
+            //to return data to a method:
+        //void = nothing returned, so change this method's void to the return's datatype
+        //return = i'm sending this variable back to the main (void) method
+        //returnedvaluevariable in main method has to match datatype
+
 
         static public void Heads_Or_Tails_Game()
         {
@@ -193,12 +229,12 @@ namespace MenuLoop
             } while (!inputString.ToUpper().Equals("Q"));
         }//eom Heads_Or_Tails_Game
 
+        //(subroutine)
         static public void Even_Or_Odd_Game()
         {
             //place any logic to do in case A
             Console.WriteLine("You chose to play Even or Odd.\n");
             int number = -1;
-            string inputString = "";
 
             while (number != 0)
             {
@@ -213,19 +249,22 @@ namespace MenuLoop
                 //condition flag
                 //NOTE: it must be reset on EACH pass of the OUTER loop
                 //the flag method is easier to understand but it requires more maintenance
-                bool validFlag = false;
-                do
-                {
-                    Console.Write("Enter a positive whole number OR enter 0 to quit.");
-                    inputString = Console.ReadLine();
-                    //you need a test to appropriately set your loop exit test
-                    if (int.TryParse(inputString, out number))
-                    {
-                        //set flag to an appropriate value to work with your logic
-                        validFlag = true;
-                    }
-                } while (validFlag == false);
-                //eow2
+                //bool validFlag = false;
+                //do
+                //{
+                //    Console.Write("Enter a positive whole number OR enter 0 to quit.");
+                //    inputString = Console.ReadLine();
+                //    //you need a test to appropriately set your loop exit test
+                //    if (int.TryParse(inputString, out number))
+                //    {
+                //        //set flag to an appropriate value to work with your logic
+                //        validFlag = true;
+                //    }
+                //} while (validFlag == false);
+                ////eow2
+                ///
+
+                number = GetIntegerInput("Enter a positive whole number OR enter 0 to quit.");
 
                 if (number > 0)
                 {
@@ -255,6 +294,54 @@ namespace MenuLoop
             }
             //eow1
         }//eom Even_Or_Odd_Game
+
+
+        //(function)
+        static public int SumofSquaresMethod(int seednumber) 
+        {
+            //the sum of squares
+            //enter positive integer number (validate)
+            //loop n times where n is the integer number (1 to n)
+            //within the loop your calculation is total = loopnumber * loopnumber
+            //after the loop, display your integer number and its sum of squares
+                //example: 
+                //entered 4
+                //do the loop 4 times
+                //1st time is 1 * 1
+                //2nd time is 2 * 2
+                //3rd time is 3 * 3
+                //4th time is 4 * 4
+            //final total = 30 (16+9+4+1=30)
+
+            int finalsquare = 0;
+            for (int loopcounter = 1; loopcounter <= seednumber; loopcounter++)
+            {
+                finalsquare += loopcounter * loopcounter;
+                //you could also use Math.Square or Math.Pow to calculate the sum of squares
+            }
+            return finalsquare;
+        }
+
+        static public int GetIntegerInput(string promptline)
+        {
+            bool validFlag = false;
+            string inputString = "";
+            int number = 0;
+
+            do
+            {
+                Console.Write(promptline);
+                inputString = Console.ReadLine();
+
+                if (int.TryParse(inputString, out number))
+                {
+                    validFlag = true;
+                }
+            } while (validFlag == false);
+
+            //if your method indicates that a return datatype is specified, you must have at least 1 return statement in your method code
+            return number;
+        }
 
     }
 }
